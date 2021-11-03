19670a537fdb0cfa4436337f512a01bb75ea509b
aleksandar] <aleksandar.vukojevicc@gmail,com>
2021-11-03 22:44:57 +0100

    monster game done

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,19 +7,27 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null,
+            logMessages: []
         }
     },
     computed: {
         playerHealthBar() {
-            return {
-                width: `${this.playerHealth}%`
+            if(this.playerHealth < 0){
+                return {
+                    width: '0'
+                }
             }
+                return { width: `${this.playerHealth}%` }
         },
         monsterHealthBar() {
-            return {
-                width: `${this.monsterHealth}%`
+            if(this.monsterHealth < 0){
+                return {
+                    width: '0'
+                }
             }
+                return { width: `${this.monsterHealth}%` }    
         },
         nthRound() {
             return this.currentRound % 3 === 0 ? false : true
@@ -27,32 +35,58 @@ const app = Vue.createApp({
     },
     watch: {
         playerHealth(vrednost) {
-            if(vrednost < 0){
-                alert('player lost')
+            if(vrednost <= 0 && this.monsterHealth <= 0){
+                this.winner = 'draw'
+            }
+            else if(vrednost <= 0){
+                this.winner = 'monster'
             }
         },
         monsterHealth(vrednost) {
-            if(vrednost < 0){
-                alert('monster lost')
+            if(vrednost <= 0 && this.playerHealth <= 0){
+                this.winner = 'draw'
+            }
+            else if(vrednost <= 0){
+                this.winner = 'player'
             }
         }
     },
     methods: {
+        newGame() {
+            this.playerHealth = 100,
+            this.monsterHealth = 100,
+            this.currentRound = 0,
+            this.winner = null,
+            this.logMessages = []
+        },
+        surrender() {
+            this.winner = 'monster'
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
+        },
         attackMonster() {
             this.currentRound++
             let attackValue = getRandomValue(5, 12)
             this.monsterHealth -= attackValue
+            this.addLogMessage('player', 'attack', attackValue)
             this.attackPlayer()
         },
         attackPlayer() {
             let attackValue = getRandomValue(8, 15)
             this.playerHealth -= attackValue
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         specialAttackMonster() {
             this.currentRound++
             let attackValue = getRandomValue(10, 25)
             this.monsterHealth -= attackValue
             this.attackPlayer()
+            this.addLogMessage('player', 'attack', attackValue)
         },
         healPlayer() {
             this.currentRound++
@@ -65,6 +99,7 @@ const app = Vue.createApp({
                 this.playerHealth += healValue
             }
             this.attackPlayer()
+            this.addLogMessage('player', 'heal', healValue)
         }
     }
 })
