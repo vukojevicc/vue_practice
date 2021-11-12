3f2e8dec71a7c9f2787452cc3305b0e0f9100063
aleksandar] <aleksandar.vukojevicc@gmail,com>
2021-11-12 20:30:15 +0100

    using v-for inside vue components

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavourite ? "(Favourite)" : "" }}</h2>
+    <button @click="toggleFavourite">Toggle favourite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+      <li><strong>Email: </strong>{{ emailAdress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAdress", "isFavourite"],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAdress: {
+      type: String,
+      required: true
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: (value) => {
+      //   return value === '1' || value === '0'
+      // }
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavourite: this.isFavourite
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavourite() {
+        this.friendIsFavourite = !this.friendIsFavourite
+    }
+  },
+};
+</script>
--- vue-cli-01-a-new-vue-project/src/main.js
+++ vue-cli-01-a-new-vue-project/src/main.js
@@ -1,3 +1,9 @@
 import { createApp } from 'vue';
+import App from './App.vue'
+import friendContact from './components/FriendContact.vue'
 
-createApp({}).mount('#app');
+const app = createApp(App);
+
+app.component('friend-contact', friendContact)
+
+app.mount('#app')
